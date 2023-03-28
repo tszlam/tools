90c0421ecd606054290cef55e0a5821e9961b579
tszlamli <tszlamli@tencent.com>
2023-03-28 12:40:43 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,38 @@
+<template>
+  <div class="common-layout">
+    <el-container class="layout-container">
+      <el-aside class="layout-aside">
+        <ElMenu class="nav-menu" router :default-active="route.path">
+          <ElMenuItem v-for="{ path, name } in routes" :key="path" :index="path" >{{ name }}</ElMenuItem>
+        </ElMenu>
+      </el-aside>
+      <el-main class="layout-main">
+        <RouterView />
+      </el-main>
+    </el-container>
+  </div>
+</template>
 <script setup lang="ts">
-import { RouterLink, RouterView, useRouter } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView, useRouter, useRoute } from 'vue-router'
 const router = useRouter()
+const route = useRoute()
 const routes = router.getRoutes().map(({ path, name }) => ({ path, name: (name as string) }))
+// TODO: 初次加载时route.path 会从/ 变成某一个，导致menu的选中项会有闪烁切换。需要看下为什么
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink v-for="{ path, name } in routes" :key="path" :to="path">{{ name }}</RouterLink>
-        <!-- <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> -->
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
+.common-layout {
+  height: 100%;
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
 }
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.common-layout .layout-container {
+  height: 100%;
+  width: 100%;
 }
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.common-layout .layout-container .layout-main {
+  background-color: var(--el-color-success-light-9);
 }
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.nav-menu {
+  height: 100%;
 }
 </style>
